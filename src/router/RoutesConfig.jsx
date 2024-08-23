3b333ae9fd80de376ce094eeb912ea74ba2d421a
Ashish Mehra <119057692+ashish280601@users.noreply.github.com>
2024-08-23 07:07:46 +0000

    added secure routing

--- src/router/RoutesConfig.jsx
+++ src/router/RoutesConfig.jsx
@@ -1,4 +1,6 @@
 import { Route, Routes, Navigate } from "react-router-dom";
+import { useState } from "react";
+import { useSelector } from "react-redux";
 
 import SignIn from "../components/auth/SignIn";
 import SignUp from "../components/auth/SignUp";
@@ -8,14 +10,18 @@ import Tasks from "../components/tasks/Tasks";
 import React from 'react'
 
 const RoutesConfig = () => {
-    // const isAuth = status;
-    const isAuth = true;
+    const [toggleAuth, setToggleAuth] = useState(true);
+    const { isSession } = useSelector((state) => state.auth);
+    // const isAuthenticated = isSession.status || toggleAuth;
+       const isAuthenticated = isSession.success;       ;
+
+    console.log("isAuthenticated", isAuthenticated);
 
     return (
         <Routes>
             <Route path="/signup" element={<SignUp />} />
-            <Route path="/login" element={<SignIn />} />
-            <Route path="/" element={isAuth ? <Dashboard /> : <Navigate to="/login" />}>
+            <Route path="/login" element={<SignIn onToggleAuth={setToggleAuth}/>} />
+            <Route path="/" element={isAuthenticated ? <Dashboard /> : <Navigate to="/login" />}>
                 <Route path="task" element={<Tasks />} />
             </Route>
         </Routes>
