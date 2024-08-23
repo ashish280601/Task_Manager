78b7c9f0347488cd2ed8fc09a34d3335dca20e9b
unknown <asmehra0628@gmail.com>
2024-08-23 21:55:55 +0530

    deplyment routing

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
   },
   server: {
-    historyApiFallback: true,  // Enable SPA Fallback
+    historyApiFallback: true,
   },
-});
+})
