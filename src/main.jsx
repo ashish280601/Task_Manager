ec486f8ed37faefb732590db9c59c1df29c74329
Ashish Mehra <119057692+ashish280601@users.noreply.github.com>
2024-08-23 07:08:33 +0000

    added toastiy with common styling

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { ToastContainer } from 'react-toastify';
 import { Provider } from 'react-redux';
+import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App.jsx';
@@ -10,10 +11,21 @@ import store from '../store.js';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ToastContainer />
     <Provider store={store}>
       <Router>
         <App />
+        <ToastContainer
+          position="bottom-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
       </Router>
     </Provider>
   </React.StrictMode>,
